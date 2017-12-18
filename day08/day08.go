c2fcdbdfd74bbf8ead47bc7cac4f09d8357d7b07
ilkin Balkanay <ilkinulas@gmail.com>
2017-12-18 08:17:57 +0300

    day09 solution

--- day08/day08.go
+++ day08/day08.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 )
 
+//TODO parse input without regex. try using fmt.Sscanf
 func Solution() (int, int) {
 	re, _ := regexp.Compile("([a-z]+) (inc|dec) (-?[0-9]+) if ([a-z]+) (([<>=!]){1,2}) (-?[0-9]+)")
 	lines := aoc.ReadLines("day08/input.txt")
--- day09/day09.go
+++ day09/day09.go
@@ -1,5 +1,86 @@
 package day09
 
+import (
+	"strings"
+	"github.com/ilkinulas/aoc2017/aoc"
+)
+
 func Solution() (int, int) {
-	return 0, 0
+	input := aoc.ReadInput("day09/input.txt")
+	return calculateScore(filterInput(input)), countGarbage(input)
+}
+
+func countGarbage(input string) int {
+	sum := 0
+	partialSum := 0
+	ignoreIndex := -1
+	collect := false
+	for i := 0; i < len(input); i++ {
+		if i == ignoreIndex {
+			continue
+		}
+		switch input[i] {
+		case '<':
+			if collect {
+				partialSum++
+			} else {
+				collect = true
+			}
+		case '>':
+			if collect {
+				sum += partialSum
+				partialSum = 0
+				collect = false
+			}
+		case '!':
+			ignoreIndex = i + 1
+		default:
+			if collect {
+				partialSum++
+			}
+		}
+	}
+	return sum
+}
+
+func filterInput(input string) string {
+	ignoreIndex := -1
+	isGarbage := false
+	var out []string
+	for i := 0; i < len(input); i++ {
+		if i == ignoreIndex {
+			continue
+		}
+		switch input[i] {
+		case '{':
+			if !isGarbage {
+				out = append(out, "{")
+			}
+		case '}':
+			if !isGarbage {
+				out = append(out, "}")
+			}
+		case '<':
+			isGarbage = true
+		case '>':
+			isGarbage = false
+		case '!':
+			ignoreIndex = i + 1
+		}
+	}
+	return strings.Join(out, "")
+}
+
+func calculateScore(s string) int {
+	sum := 0
+	incr := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '{' {
+			sum += incr
+			incr--
+		} else if s[i] == '}' {
+			incr++
+		}
+	}
+	return sum
 }
